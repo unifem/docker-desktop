459a22d592b7aaad8fb91a1c66e2b95ea4ce6047
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-04 19:03:33 -0400

    Changed to use dev branch for numgeom_jupyter

--- numgeom_jupyter.py
+++ numgeom_jupyter.py
@@ -35,9 +35,9 @@ def parse_args(description):
                         default=APP + "/desktop")
 
     parser.add_argument('-t', '--tag',
-                        help='Tag of the image. The default is latest. ' +
+                        help='Tag of the image. The default is dev. ' +
                         'If the image already has a tag, its tag prevails.',
-                        default="latest")
+                        default="dev")
 
     parser.add_argument('-p', '--pull',
                         help='Pull the latest Docker image. ' +
