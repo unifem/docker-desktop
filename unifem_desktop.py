e02f663404079510ff11863f8ea2d0c6bb6fe474
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-16 14:12:53 -0400

    Changed default for args to []

--- unifem_desktop.py
+++ unifem_desktop.py
@@ -80,7 +80,8 @@ def parse_args(description):
                         help='All the arguments after -a will be passed to the ' +
                         '"docker run" command. Useful for specifying ' +
                         'resources and environment variables.',
-                        nargs=argparse.REMAINDER)
+                        nargs=argparse.REMAINDER,
+                        default=[])
 
     args = parser.parse_args()
 
